5856f4cb1581e0bcd5f15b601d7e340116f1c758
Antonio Lago <antonio.lago@gmail.com>
2022-10-02 19:32:01 +0200

    Actualizado a salida en forma de tabla

--- main.py
+++ main.py
@@ -2,18 +2,10 @@ from PyQt5.QtWidgets import QApplication, QStyleFactory
 from ui.totalizador import clasePrincipal
 
 import sys
-'''
-def run():
-    app = QApplication(sys.argv)
-    ui = clasePrincipal()
-    ui.show()
-    #sys.exit(app.exec_())
-    app.exec_()
-run()
-'''
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    app.setStyle(QStyleFactory.create('Fussion')) # won't work on windows style.
+    app.setStyle(QStyleFactory.create('Fussion'))
     ui = clasePrincipal()
     ui.show()
     app.exec_()
